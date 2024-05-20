0a6a7214c027ee71eabf780472f3469ea27cef94
avigyabb <avigyabb@stanford.edu>
2024-05-19 23:49:44 -0700

    model knows chat history

--- backend/myapi/chat.py
+++ backend/myapi/chat.py
@@ -1,3 +1,8 @@
+from .models import ChatHistory
+
 def respond_to_message (llm, message, documents):
-    system_prompt = """The user said: \n {} \n and provided the following documents: \n {}. \n Respond to the user's query/instructions: """.format(message, documents)
+    all_messages = ChatHistory.objects.all()
+    message_list = [f"User: {message.user}, Message: {message.message}" for message in all_messages]
+    message_string = "\n".join(message_list)
+    system_prompt = """The user said: \n {message} \n and provided the following documents: \n {documents}. The user's previous chat history is: \n {message_string}\n Respond to the user's query/instructions which is: """.format(message=message, documents=documents, message_string=message_string)
     return llm.invoke(system_prompt)
