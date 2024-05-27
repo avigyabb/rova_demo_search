8784dd2dfab9ae88197f30142f9e0835604c753c
joshsanyal <josh.sanyal@yahoo.com>
2024-05-27 15:30:52 -0700

    Make the sources content font smaller

--- frontend/src/Components/Sources.js
+++ frontend/src/Components/Sources.js
@@ -3,20 +3,20 @@ import {useState} from "react";
 const Sources = ({documents}) => {
 
     return (
-        <div style = {{
-            backgroundColor : "#f0f0f0",
-            padding : "20px",
-            width : "300px",
-            overflowY : "auto",
+        <div style={{
+            backgroundColor: "#f0f0f0",
+            padding: "20px",
+            width: "300px",
+            overflowY: "auto",
         }}>
-            <h1 className = "text-xl" style = {{marginBottom : "20px"}}> Sources </h1>
-            {documents.length > 0 ? documents.map ((document, index) => (
-                <div key = {index} style = {{marginBottom : "10px"}}> 
-                <strong style = {{marginBottom : "10px"}}> {document["name"]} </strong>
-                <p> {document["content"]} </p>
+            <h1 className="text-xl" style={{marginBottom: "20px"}}>Sources</h1>
+            {documents.length > 0 ? documents.map((document, index) => (
+                <div key={index} style={{marginBottom: "10px"}}>
+                    <strong style={{marginBottom: "10px"}}>{document["name"]}</strong>
+                    <p style={{fontSize: "12px"}}>{document["content"]}</p>
                 </div>
             )) :
-            <strong> No content from uploaded files was retrieved for this response. </strong>}
+                <strong>No content from uploaded files was retrieved for this response.</strong>}
         </div>
     )
 }
