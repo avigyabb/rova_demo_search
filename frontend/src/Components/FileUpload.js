e2bb5e35ff9bbd4703419deb46aa67f9dbab5b2d
joshsanyal <josh.sanyal@yahoo.com>
2024-05-29 22:19:56 -0700

    Intermediate popup for document upload

--- frontend/src/Components/FileUpload.js
+++ frontend/src/Components/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FaTrash, FaTags, FaPlus } from 'react-icons/fa';
 import "../Styles/FileUpload.css"
 import { REACT_APP_API_URL } from "../consts";
@@ -15,6 +15,7 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
   const [files, setFiles] = useState([]);
   const [pdfUrl, setPdfUrl] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
+  const [showUploadPopup, setShowUploadPopup] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [showOverlay, setShowOverlay] = useState(false);
   const [inputs, setInputs] = useState(['']);
@@ -23,6 +24,8 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
   const granteeFiles = files.filter(file => file.file_organization === 'grantee');
   const [focusedFile, setFocusedFile] = useState(null);
   const [focusedFileOrganization, setFocusedFileOrganization] = useState(null);
+  const fileInputRef = useRef(null);
+  const popupFileInputRef = useRef(null);
 
   const fetchFiles = async () => {
     try {
@@ -99,39 +102,39 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
         }
         return;
     } else {
-        const newFiles = Array.from(event.target.files);
+        const newFiles = Array.from(event.target.files);    
         for (const file of newFiles) {
-        const formData = new FormData();
-        formData.append('file', file);
-        formData.append('selectedFileIds', JSON.stringify(selectedFileIds));
-        formData.append('questions', JSON.stringify(inputs));
-        formData.append('chat_session', JSON.stringify(selectedSession.id));
-        formData.append('file_organization', fileOrganization);
-        try {
-            const response = await fetch(REACT_APP_API_URL + `upload/${isGrantApp}/`, {
-              method: 'POST',
-              body: formData,
-            });
-            if (isGrantApp) {
-                const blob = await response.blob();
-                const url = URL.createObjectURL(blob);
-                setPdfUrl(url);
-                setShowPopup(true);
-            } else {
-                const result = await response.json();
-                console.log(result);
-                if(!('error' in result)) {
-                    setFiles((prevFiles) => [...prevFiles, result]);
-                    setSelectedFileIds(prevIds => [...prevIds, result.id]); // Auto-select new file
-                }
-            }
-            
-            } catch (error) {
-                console.error('Error uploading files:', error);
-            } finally {
-                setIsLoading(false);
-            }
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('selectedFileIds', JSON.stringify(selectedFileIds));
+      formData.append('questions', JSON.stringify(inputs));
+      formData.append('chat_session', JSON.stringify(selectedSession.id));
+      formData.append('file_organization', fileOrganization);
+      try {
+        const response = await fetch(REACT_APP_API_URL + `upload/${isGrantApp}/`, {
+          method: 'POST',
+          body: formData,
+        });
+        if (isGrantApp) {
+          const blob = await response.blob();
+          const url = URL.createObjectURL(blob);
+          setPdfUrl(url);
+          setShowPopup(true);
+        } else {
+          const result = await response.json();
+          console.log(result);
+          if(!('error' in result)) {
+            setFiles((prevFiles) => [...prevFiles, result]);
+            setSelectedFileIds(prevIds => [...prevIds, result.id]); // Auto-select new file
+          }
         }
+
+      } catch (error) {
+        console.error('Error uploading files:', error);
+      } finally {
+        setIsLoading(false);
+      }
+      }
     }
   };
 
@@ -146,7 +149,7 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
   const handleTagChange = async (fileId, newOrg) => {
     const formData = new FormData();
     formData.append('file_organization', newOrg);
-        
+
     try {
       const response = await fetch(REACT_APP_API_URL + `edit/${fileId}/`, {
         method: 'POST',
@@ -164,26 +167,20 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
       setFocusedFileOrganization(null);
     }
   }
-  
+
   return (
-      <div style={{ 
-        width: '300px', 
-        backgroundColor: '#f0f0f0', 
-        padding: '15px', 
-        borderRight: '1px solid black', 
-        overflowY: 'auto',
-      }}>
-        <div style = {{display : "flex", flexDirection : "column", height : "100%" }}>
+    <div style={{
+      width: '300px',
+      backgroundColor: '#f0f0f0',
+      padding: '15px',
+      borderRight: '1px solid black',
+      overflowY: 'auto',
+    }}>
+      <div style = {{display : "flex", flexDirection : "column", height : "100%" }}>
         <div style={{ marginTop: '40px', flexGrow : 1, overflowY : "auto" }}>
-          <label className="add-file-btn">
+          <label className="add-file-btn" onClick={() => setShowUploadPopup(true)}>
             <FaPlus style={{ marginRight: '8px' }}/>
-            Upload Files
-            <input
-              type="file"
-              multiple
-              onChange={(event) => handleUpload(event, 0, 'reference')}
-              style={{ display: 'none' }}
-            />
+            Upload Documents
           </label>
 
           {files.length === 0 ? (
@@ -191,28 +188,28 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
           ) : (
             <ul style={{ paddingLeft: '0px', marginTop: '10px' }}>
               {files.map((file) => (
-                <li 
-                  key={file.id} 
+                <li
+                  key={file.id}
                   onMouseEnter={() => setFocusedFile(file.id)}
                   onMouseLeave={() => setFocusedFile(null)}
-                  style={{ 
+                  style={{
                     backgroundColor: focusedFile === file.id && 'lightgray',
-                    padding: '10px', 
-                    borderRadius: '5px', 
-                    marginBottom: '5px' 
+                    padding: '10px',
+                    borderRadius: '5px',
+                    marginBottom: '5px'
                   }}
                 >
                   {focusedFileOrganization !== file.id && (
                     <div style={{ display: 'flex' }}>
-                      <p style={{ 
-                        backgroundColor: organizationColors[file.file_organization], 
-                        color: 'white', 
-                        padding: '3px', 
-                        paddingRight: '10px', 
-                        paddingLeft: '10px', 
-                        borderRadius: '30px', 
-                        fontSize: '10px', 
-                        marginBottom: '2px', 
+                      <p style={{
+                        backgroundColor: organizationColors[file.file_organization],
+                        color: 'white',
+                        padding: '3px',
+                        paddingRight: '10px',
+                        paddingLeft: '10px',
+                        borderRadius: '30px',
+                        fontSize: '10px',
+                        marginBottom: '2px',
                         marginLeft: '20px'
                       }}>
                         {focusedFile === file.id ? file.file_organization : ''}
@@ -229,16 +226,16 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
                     <span style={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontSize: '14px' }}>{file.filename}</span>
                     {focusedFile === file.id && (
                       <>
-                      <FaTags
-                        size={22}
-                        className='tags-btn'
-                        onClick={() => setFocusedFileOrganization(focusedFileOrganization === file.id ? null : file.id)}
-                      />
-                      <FaTrash 
-                        size={20}
-                        className='delete-btn'
-                        onClick={() => handleDelete(file.id)} 
-                      />
+                        <FaTags
+                          size={22}
+                          className='tags-btn'
+                          onClick={() => setFocusedFileOrganization(focusedFileOrganization === file.id ? null : file.id)}
+                        />
+                        <FaTrash
+                          size={20}
+                          className='delete-btn'
+                          onClick={() => handleDelete(file.id)}
+                        />
                       </>
                     )}
                   </div>
@@ -256,40 +253,41 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
               ))}
             </ul>
           )}
-          </div>
+        </div>
 
         <div style={{ position : "sticky", bottom: '0', width: '100%', margin: '20px' }}>
-        <label style={{ background: "lightblue", color: "black" }} className="custom-file-input">
+          <label style={{ background: "lightblue", color: "black" }} className="custom-file-input">
             {isLoading ? (
-                <div style={{color: 'white'}}>
+              <div style={{color: 'white'}}>
                 <CircularProgress color="inherit"/>
-                </div>
+              </div>
             ) : (
-                <div>
+              <div>
                 <button onClick={toggleOverlay}>Draft Grant</button>
                 {showOverlay && (
-                <Form
+                  <Form
                     onClose={toggleOverlay}
                     handleUpload={handleUpload}
                     submitForm={submitForm}
                     inputs={inputs}
                     setInputs={setInputs}
-                />
+                  />
                 )}
-                </div>
+              </div>
             )}
             <input
-                type="file"
-                multiple
-                onChange={(event) => handleUpload(event, 1, 'template')}
-                style={{ display: 'none' }}
+              type="file"
+              ref={fileInputRef}
+              multiple
+              onChange={(event) => handleUpload(event, 1, 'template')}
+              style={{ display: 'none' }}
             />
-        </label>
-    </div>
+          </label>
+        </div>
 
-    </div>
+      </div>
 
-    {showPopup && (
+      {showPopup && (
         <div style={{
           position: 'fixed',
           top: '47.5%',
@@ -320,6 +318,64 @@ const FileUploadComponent = ({ selectedSession, selectedFileIds, setSelectedFile
           </a>
         </div>
       )}
+
+      {showUploadPopup && (
+        <div style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '300px',
+          height: '200px',
+          backgroundColor: 'white',
+          boxShadow: '0 4px 8px rgba(0,0,0,0.2)',
+          zIndex: 1000,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}>
+          <button
+            style={{
+              padding: '10px 20px',
+              backgroundColor: '#007bff',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => {
+              setShowUploadPopup(false);
+              if (popupFileInputRef.current) {
+                popupFileInputRef.current.click();
+              }
+            }}
+          >
+            Upload Files
+          </button>
+          <button
+            style={{
+              marginTop: '20px',
+              padding: '10px 20px',
+              backgroundColor: '#6c757d',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => setShowUploadPopup(false)}
+          >
+            Close
+          </button>
+        </div>
+      )}
+      <input
+        type="file"
+        ref={popupFileInputRef}
+        multiple
+        onChange={(event) => handleUpload(event, 0, 'reference')}
+        style={{ display: 'none' }}
+      />
     </div>
   );
 };
